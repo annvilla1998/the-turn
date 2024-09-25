4ed1279d382bb574d69357b7c60c885d1c0d8187
annvilla1998 <villalobos1998@gmail.com>
2024-09-25 14:35:16 -0700

    Configure database

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+import path from "path";
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+    prependData: `@import "./base.scss";`
+  }
 };
 
 export default nextConfig;
---
+++ prisma/index.js
@@ -0,0 +1,33 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const users = [
+  {
+    name: "Daniel",
+    email: "theturnvv@gmail.com",
+    password: "0£0l6=gPm2Bo",
+    owner: true,
+  },
+];
+
+async function main() {
+  console.log("Seeding users...");
+  for (const u of users) {
+    const user = await prisma.user.create({
+      data: u,
+    });
+    console.log(`Created user with id ${user.id}`);
+  }
+  console.log("Done seeding.");
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
