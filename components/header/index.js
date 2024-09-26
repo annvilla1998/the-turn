97ce528ff843aa2c0a9050dbb1cea1b47dca8a33
annvilla1998 <villalobos1998@gmail.com>
2024-09-26 13:25:49 -0700

    Add navigation

--- components/header/index.js
+++ components/header/index.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import styles from  './styles.module.scss';
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header>
-      <nav>
-        
+      <nav className={styles.nav}>
+          <img src="" alt="The TurnVV" className={styles.nav__logo} />
+          <ul className={styles.nav__list}>
+            <li><Link href="/reserve">Book Now</Link></li>
+            <li><Link href="/memberships">Memberships</Link></li>
+            <li><Link href="/events">Events</Link></li>
+            <li><Link href="/about">About us</Link></li>
+            <li><Link href="/gift-cards">Gift Cards</Link></li>
+            <li><Link href="/contact">Contact Us</Link></li>
+          </ul>
       </nav>
     </header>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Header from "@/components/header";
-import styles from "@/styles/Home.module.css";
+import styles from "@/styles/Home.module.scss";
 
 export default function Home() {
   return (
