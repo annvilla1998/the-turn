358685ff41c2630756cafa363cee74c0e783e9b7
annvilla1998 <villalobos1998@gmail.com>
2024-10-03 23:31:26 -0700

    Add calendar

--- pages/the-turn/book.js
+++ pages/the-turn/book.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '@/styles/Book.module.scss';
 import SecondaryHeader from '@/components/layouts/SecondaryHeader';
-
+import Calendar from 'react-calendar';
 export default function Book() {
-  return <div>Book</div>;
+  const [value, onChange] = useState(new Date());
+
+  return (
+    <div>
+      <Calendar onChange={onChange} value={value} />
+    </div>
+  );
 }
 
 Book.getLayout = function getLayout(page) {
