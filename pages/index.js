8c204307fac84faabd7b7b44af3f183adda1b13b
annvilla1998 <villalobos1998@gmail.com>
2025-03-13 21:55:20 -0700

    Add map

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import styles from "@/styles/Home.module.scss";
 import MainHeader from "@/components/layouts/MainHeader";
 import { ImageCarousel } from "@/components/image-carousel";
 import HourlyRates from "@/components/hourly-rates";
+import { Box } from "@mui/system";
 
 function Home() {
   return (
@@ -45,6 +46,19 @@ function Home() {
       </section>
       <section>
         <ImageCarousel />
+      </section>
+      <section>
+      <Box sx={{ width: "100%" }}>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13157.314576223032!2d-117.36524167366714!3d34.46918626984016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c366223d5c6baf%3A0x8bd8fae0adc9699c!2s12044%20Dunia%20Rd%20F%2C%20Victorville%2C%20CA%2092392!5e0!3m2!1sen!2sus!4v1741927695856!5m2!1sen!2sus"
+        style={{
+          width: "100%",
+          height: "400px",
+          border: "none",
+        }}
+        allowFullScreen
+      />
+    </Box>
       </section>
     </div>
   );
